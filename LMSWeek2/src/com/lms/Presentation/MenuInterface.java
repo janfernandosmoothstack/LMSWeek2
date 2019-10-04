4ecd9efbcacc2c2089e075d63601bd1f933860e4
janet <janet@Janet>
2019-10-04 17:19:12 -0400

    menu interface

--- LMSWeek2/src/com/lms/Presentation/MenuInterface.java
+++ LMSWeek2/src/com/lms/Presentation/MenuInterface.java
@@ -7,6 +7,19 @@ public interface MenuInterface {
 	
 	public void showMenu();
 	
+	public static void crudMenu() {
+		String menu = 
+				"1. Create\n" +
+				"2. Update\n" +
+				"3. Delete\n" +
+				"4. View" +
+				"5. Exit\n" +
+				"\n" +
+				"Please select an option(1-3):";
+		
+		System.out.println(menu);
+	}
+	
 	public static int readInt() {
 		int inputNum = input.nextInt();
 		return inputNum;
