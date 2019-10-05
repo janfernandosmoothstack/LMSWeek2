f5f58d04714ae88eb32a6d953c9b7f4ae10077ea
Sayana Shrestha <sayana.shrestha@smoothstack.com>
2019-10-05 18:44:25 -0400

    Please review my user interface.

--- LMSWeek2/src/com/lms/DAO/BorrowerDAO.java
+++ LMSWeek2/src/com/lms/DAO/BorrowerDAO.java
@@ -3,7 +3,6 @@ package com.lms.DAO;
 import java.sql.*;
 
 import com.lms.Presentation.BorrowerUserMenu;
-import com.mysql.jdbc.ResultSetMetaData;
 
 public class BorrowerDAO {
        public static void main(String[] args) {
@@ -12,27 +11,9 @@ public class BorrowerDAO {
                Class.forName("com.mysql.jdbc.Driver");
               
                Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","TapHouse123");
-             
-               String sql = "select * from tbl_borrower";
+               BorrowerUserMenu borrower = new BorrowerUserMenu();
+               borrower.readCardNo(con);
               
-                 //executeQuesry for select and executeUpdate for insert, delete, update  or any change
-               
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery(sql);
-                //just for displaying 
-                java.sql.ResultSetMetaData rsmd = rs.getMetaData();
-                int columnsNumber = rsmd.getColumnCount();
-            
-            while (rs.next())
-            {
-                //Print one row
-                for(int i = 1 ; i <= columnsNumber; i++)
-                {
-                      System.out.print(rs.getString(i) + "\t "); //Print one element of a row
-                }
-                  System.out.println();//Move to the next line to print the next row.
-            }
-                           con.close(); //close connection
         }
                    catch(Exception e)
                 {
--- LMSWeek2/src/com/lms/Presentation/BorrowerUserMenu.java
+++ LMSWeek2/src/com/lms/Presentation/BorrowerUserMenu.java
@@ -1,6 +1,11 @@
 package com.lms.Presentation;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class BorrowerUserMenu {
 	
@@ -8,47 +13,157 @@ public class BorrowerUserMenu {
 		
 		System.out.print("Please enter your Card No:  " );
 		int cardNo = MenuInterface.readInt();
-		
+		PreparedStatement ps = null;
 		//validation if card No exists
-		//if cardNo
+		try {
+				ps = con.prepareStatement("SELECT cardNo FROM tbl_borrower WHERE cardNo = ? ");
+				ps.setInt (1, cardNo);
+				ResultSet rs = ps.executeQuery();
+				if (rs.next()) {
+					showMenu(con, cardNo);
+				} else {
+					System.out.println("Invalid input ");
+					readCardNo(con);
+				}
+		}
+		   catch(Exception e)
+        {
+            System.out.println(e);
 		
-		showMenu(con, cardNo);
-		
+		}
 	}
 	
 	public void showMenu(Connection con, int cardNo) {
-		String choice = "";
+		int choice = 0;
 		
 		do {
-			System.out.println("Borrower Menu");
 			System.out.println ("1) Checkout a book");
 			System.out.println ("2) Return a book");
-			System.out.println ("3)	Quit to Previous ");
-			System.out.print ("Please enter your choice here (number): ");
+			System.out.println ("3) Back to Previous Main Menu");
+			System.out.println ("Please enter your choice here: ");
 			
 			boolean checkChoice = false;
 			
 			//loop until user enters a valid choice
 			while(checkChoice != true) {
-				choice = MenuInterface.readString();
+				choice = MenuInterface.readInt();
 				
 				switch(choice) {
-					case "1": //Checkout
+					case 1: //checkout
+						DisplayBranchCheckout(con, cardNo);
 						checkChoice = true;
 						break;
-					case "2": //Return
+					case 2: //Return
+						DisplayBranchReturn(con, cardNo);
 						checkChoice = true;
 						break;
 					
-					case "3": //Exit
+					case 3:
+					//takes to Main Menu
 						return;
+						
 					default:
 						System.out.println("Please enter a valid option.");
 						break;
 				}
 			}
-		}while(choice != "3");
+		}while(choice != 3);
 	}
 	
+	public void DisplayBranchCheckout(Connection con, int cardNo) {
+        try {
+            Statement st = con.createStatement();
+           ResultSet rs = st.executeQuery("select branchId, branchName from tbl_library_branch");
+           ResultSetMetaData rsmd = rs.getMetaData();
+           int columnsNumber = rsmd.getColumnCount();
+           System.out.println("Branch ID \t Branch Name");
+           
+           while (rs.next())
+             {
+                 //Print one row
+                 for(int i = 1 ; i <= columnsNumber; i++)
+                 {
+                       System.out.print(rs.getString(i) + "\t\t "); //Print one element of a row
+                 }
+                   System.out.println();//Move to the next line to print the next row.
+             }
+           System.out.println("0)Quit to cancel operation ");
+           System.out.print("Pick the Branch Id you want to checkout from:  " );
+    		 int bhId = MenuInterface.readInt();
+    		 
+    		 if (bhId == 0) {
+    			 showMenu(con, cardNo);
+    		 }
+    		 else {
+    			 DisplayBranchBook(con, cardNo, bhId); 
+    		 }
+                              
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    }
+	public void DisplayBranchReturn(Connection con, int cardNo) {
+        try {
+            Statement st = con.createStatement();
+           ResultSet rs = st.executeQuery("select branchId, branchName from tbl_library_branch");
+           ResultSetMetaData rsmd = rs.getMetaData();
+           int columnsNumber = rsmd.getColumnCount();
+           System.out.println("Branch ID \t Branch Name");
+           
+           while (rs.next())
+             {
+                 //Print one row
+                 for(int i = 1 ; i <= columnsNumber; i++)
+                 {
+                       System.out.print(rs.getString(i) + "\t\t "); //Print one element of a row
+                 }
+                   System.out.println();//Move to the next line to print the next row.
+             }
+           System.out.println("0)Quit to cancel operation ");
+           System.out.print("Pick the Branch Id you want to return to:  " );
+    		 int bhId = MenuInterface.readInt();
+    		 
+    		 if (bhId == 0) {
+    			 showMenu(con, cardNo);
+    		 }
+    		 else {
+    			 
+    		 }
+                              
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    }
+	
+	public void DisplayBranchBook(Connection con, int cardNo, int bhId) {
+        try {
+           PreparedStatement ps = con.prepareStatement("SELECT tbl_book.bookId, CONCAT(tbl_book.title, ' by ' , tbl_author.authorName)  FROM tbl_book INNER JOIN tbl_author ON tbl_book.authId = tbl_author.authorId INNER JOIN tbl_book_copies ON tbl_book.bookId =  tbl_book_copies.bookId INNER JOIN tbl_library_branch ON tbl_book_copies.branchId = tbl_library_branch.branchId WHERE tbl_library_branch.branchId = ? AND tbl_book_copies.noOfCopies >=1 ");
+          
+        	ps.setInt (1, bhId);
+            ResultSet rs = ps.executeQuery();
+           ResultSetMetaData rsmd = rs.getMetaData();
+           int columnsNumber = rsmd.getColumnCount();
 
+           System.out.println("Book ID \t Book name by Author");
+           while (rs.next())
+             {
+                 //Print one row
+                 for(int i = 1 ; i <= columnsNumber; i++)
+                 {
+                       System.out.print(rs.getString(i) + "\t\t "); //Print one element of a row
+                 }
+                   System.out.println();//Move to the next line to print the next row.
+             }
+                              
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Book ID \t Book name by Author");
+          
+    }
+	
 }
