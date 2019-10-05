0ab1f2a26fdecd44b3a2ab7356509d6fce1b15e8
janet <janet@Janet>
2019-10-05 16:35:16 -0400

    DAO Commit

--- LMSWeek2/src/com/lms/test/Test.java
+++ LMSWeek2/src/com/lms/test/Test.java
@@ -10,7 +10,7 @@ public class Test {
 	public static void main(String[] args) {
 		Connection con = null;
 		try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","**********");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","********");
             
             MainMenu mainMenu = new MainMenu();
     		mainMenu.showMenu(con);
